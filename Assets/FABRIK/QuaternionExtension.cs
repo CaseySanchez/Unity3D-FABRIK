a1b268f1bc7d561943571d1cedf4e1131ee7f588
Tannz0rz <nabbz0rz@gmail.com>
2019-02-24 15:52:49 -0500

    Create QuaternionExtension.cs

---
+++ Assets/FABRIK/QuaternionExtension.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuaternionExtension
+{
+    public static Quaternion Normalize(this Quaternion quaternion)
+    {
+        float dot = quaternion.x * quaternion.x + quaternion.y * quaternion.y + quaternion.z * quaternion.z + quaternion.w * quaternion.w;
+        float invLength = 1.0F / Mathf.Sqrt(dot);
+
+        quaternion.x *= invLength;
+        quaternion.y *= invLength;
+        quaternion.z *= invLength;
+        quaternion.w *= invLength;
+
+        return quaternion;
+    }
+
+    public static void Decompose(this Quaternion quaternion, Vector3 direction, out Quaternion swing, out Quaternion twist)
+    {
+        Vector3 vector = new Vector3(quaternion.x, quaternion.y, quaternion.z);
+        Vector3 projection = Vector3.Project(vector, direction);
+
+        twist = new Quaternion(projection.x, projection.y, projection.z, quaternion.w).Normalize();
+        swing = quaternion * Quaternion.Inverse(twist);
+    }
+
+    public static Quaternion Constrain(this Quaternion quaternion, float angle)
+    {
+        float magnitude = Mathf.Sin(0.5F * angle);
+        float sqrMagnitude = magnitude * magnitude;
+
+        Vector3 vector = new Vector3(quaternion.x, quaternion.y, quaternion.z);
+
+        if (vector.sqrMagnitude > sqrMagnitude)
+        {
+            vector = vector.normalized * magnitude;
+
+            quaternion.x = vector.x;
+            quaternion.y = vector.y;
+            quaternion.z = vector.z;
+            quaternion.w = Mathf.Sqrt(1.0F - sqrMagnitude) * Mathf.Sign(quaternion.w);
+        }
+
+        return quaternion;
+    }
+}
