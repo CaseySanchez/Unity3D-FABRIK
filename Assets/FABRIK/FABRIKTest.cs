5e27b4f1d64244290bc10e456b214b0bbdd5c310
Tannz0rz <nabbz0rz@gmail.com>
2019-03-22 18:26:25 -0400

    Update FABRIKTest.cs

--- Assets/FABRIK/FABRIKTest.cs
+++ Assets/FABRIK/FABRIKTest.cs
@@ -3,24 +3,18 @@ using System.Collections;
 
 public class FABRIKTest : FABRIK
 {
-    public Transform targetTransformA;
-    public Transform targetTransformB;
-    public Transform targetTransformC;
-    public Transform targetTransformD;
+    public Transform sphere_right;
+    public Transform sphere_left;
 
     public override void OnFABRIK ()
     {
         float speed = 10.0F;
         float step = Time.deltaTime * speed;
 
-        FABRIKChain up_left = GetEndChain("up_left");
-        FABRIKChain down_left = GetEndChain("down_left");
-        FABRIKChain up_right = GetEndChain("up_right");
-        FABRIKChain down_right = GetEndChain("down_right");
+        FABRIKChain right = GetEndChain("right");
+        FABRIKChain left = GetEndChain("left");
 
-        up_left.Target = Vector3.MoveTowards(up_left.EndEffector.Position, targetTransformA.position, step);
-        down_left.Target = Vector3.MoveTowards(down_left.EndEffector.Position, targetTransformB.position, step);
-        up_right.Target = Vector3.MoveTowards(up_right.EndEffector.Position, targetTransformC.position, step);
-        down_right.Target = Vector3.MoveTowards(down_right.EndEffector.Position, targetTransformD.position, step);
+        right.Target = Vector3.MoveTowards(right.EndEffector.Position, sphere_right.position, step);
+        left.Target = Vector3.MoveTowards(left.EndEffector.Position, sphere_left.position, step);
     }
 }
