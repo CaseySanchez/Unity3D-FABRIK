0e5561e9520d275463911eb2c460c90f0391a9d2
Tannz0rz <nabbz0rz@gmail.com>
2019-03-27 17:12:12 -0700

    Add files via upload

--- Assets/FABRIK/FABRIKTest.cs
+++ Assets/FABRIK/FABRIKTest.cs
@@ -8,11 +8,11 @@ public class FABRIKTest : FABRIK
 
     public override void OnFABRIK ()
     {
-        float speed = 10.0F;
+        float speed = 100.0F;
         float step = Time.deltaTime * speed;
 
-        FABRIKChain right = GetEndChain("right");
-        FABRIKChain left = GetEndChain("left");
+        FABRIKChain right = GetEndChain("right_end_effector");
+        FABRIKChain left = GetEndChain("left_end_effector");
 
         right.Target = Vector3.MoveTowards(right.EndEffector.Position, sphere_right.position, step);
         left.Target = Vector3.MoveTowards(left.EndEffector.Position, sphere_left.position, step);
